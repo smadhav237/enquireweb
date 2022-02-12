7ca4ef726c9b64bb122587338c435cb476482666
madhavpcm <dpmadhav@hotmail.com>
2022-02-12 23:12:35 +0530

    index body and div loading working

---
+++ index.js
@@ -0,0 +1,24 @@
+function loadHTML(id, filename){
+    console.log(`div id: ${id}, filename: ${filename}`);
+    
+    let xhttp;
+    let element = document.getElementById(id);
+    let file =filename;
+    
+    if(file){
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function (){
+            if (this.readyState == 4){
+                if(this.status == 200){
+                    element.innerHTML = this.responseText;
+                }
+                if(this.status == 404){
+                    element.innerHTML = "<h1> Page Not Found </ht>";
+                }
+            }
+        }
+        xhttp.open("GET", `${file}`, true);
+        xhttp.send();
+        return;
+    }
+}
